69f47f86ec2e3c8640f8776bf98a7d670bd99d62
Saulo Vargas <sauloco_022@hotmail.com>
2016-03-05 22:42:36 -0300

    Commit inicial

---
+++ WindowsApplication1/Presentacion.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Presentacion
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Presentacion))
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.AxAcroPDF1 = New AxAcroPDFLib.AxAcroPDF()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AxAcroPDF1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(12, 12)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(270, 218)
+        Me.AxWindowsMediaPlayer1.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(288, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(257, 216)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.AllowWebBrowserDrop = False
+        Me.WebBrowser1.Location = New System.Drawing.Point(12, 236)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.Size = New System.Drawing.Size(270, 250)
+        Me.WebBrowser1.TabIndex = 2
+        Me.WebBrowser1.Url = New System.Uri("", System.UriKind.Relative)
+        Me.WebBrowser1.WebBrowserShortcutsEnabled = False
+        '
+        'AxAcroPDF1
+        '
+        Me.AxAcroPDF1.Enabled = True
+        Me.AxAcroPDF1.Location = New System.Drawing.Point(288, 236)
+        Me.AxAcroPDF1.Name = "AxAcroPDF1"
+        Me.AxAcroPDF1.OcxState = CType(resources.GetObject("AxAcroPDF1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxAcroPDF1.Size = New System.Drawing.Size(257, 250)
+        Me.AxAcroPDF1.TabIndex = 4
+        '
+        'Presentacion
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(563, 500)
+        Me.Controls.Add(Me.AxAcroPDF1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Presentacion"
+        Me.Text = "Presentacion"
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AxAcroPDF1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents AxAcroPDF1 As AxAcroPDFLib.AxAcroPDF
+End Class
---
+++ WindowsApplication1/Presentacion.vb
@@ -0,0 +1,39 @@
+﻿Public Class Presentacion
+    Public WebBrowserBiblia As Boolean = False
+    Public MostrarCursor As Boolean = True
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+        Dim VersiculoEncontrado As Boolean = False
+        If WebBrowserBiblia Then
+            Dim PageElement As HtmlElementCollection = Me.WebBrowser1.Document.GetElementsByTagName("div")
+            For Each CurElement As HtmlElement In PageElement
+                If (CurElement.GetAttribute("className") = "results") Then
+                    Dim style As String = ".results ul{list-style-type:none!important}ul.results{list-style:none}.results .caption{color:#443;font-size:3em;font-weight:300;padding-top:.5em;padding-bottom:0}.dir-ltr .results .caption,.results .caption.dir-ltr{padding-left:0;padding-right:0}.results .caption .lnk{color:#4477a1}.results a{text-decoration:none}.lang-es,.lang-es.lib-es,.mepslang-S.meps-S.meps_S{font-family:Helvetica,Arial,sans-serif;font-size:2em}body{color:#333;background-color:#f7f7f5;}p{font-size: 2em;}"
+                    Dim ToBrowser As String = "<html><head><style>" + style + "</style></head><body><div Class= results >" + CurElement.InnerHtml + "</div></body></html>"
+                    WebBrowser1.DocumentText = ToBrowser
+                    VersiculoEncontrado = True
+                End If
+            Next
+            If Not VersiculoEncontrado Then
+                WebBrowser1.DocumentText = "Versículo no encontrado."
+            End If
+            WebBrowserBiblia = False
+        End If
+    End Sub
+
+    'Private Sub Form1_MouseEnter(sender As Object, e As EventArgs) Handles MyBase.MouseEnter, PictureBox1.MouseEnter ', AxAcroPDF1.MouseEnter ' ', AxWindowsMediaPlayer1.MouseEnter , WebBrowser1.MouseEnter 
+    '    If Not MostrarCursor Then
+    '        Cursor.Hide()
+    '    Else
+    '        Cursor.Show()
+    '    End If
+
+    'End Sub
+
+    'Private Sub Form1_MouseLeave(sender As Object, e As EventArgs) Handles MyBase.MouseLeave, PictureBox1.MouseLeave ', AxAcroPDF1.MouseLeave ', AxWindowsMediaPlayer1.MouseLeave ', WebBrowser1.MouseLeave 
+    '    If Not MostrarCursor Then
+    '        Cursor.Show()
+    '    Else
+    '        Cursor.Hide()
+    '    End If
+    'End Sub
+End Class
